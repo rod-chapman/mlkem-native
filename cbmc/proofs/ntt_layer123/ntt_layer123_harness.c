e05a4367670b37d1bf2e87b852857190263d2fc8
Rod Chapman <rodchap@amazon.com>
2024-12-03 11:49:41 +0000

    Adds proof of ntt_layer123() function
    Signed-off-by: Rod Chapman <rodchap@amazon.com>

---
+++ cbmc/proofs/ntt_layer123/ntt_layer123_harness.c
@@ -0,0 +1,30 @@
+// Copyright (c) 2024 The mlkem-native project authors
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+/*
+ * Insert copyright notice
+ */
+
+/**
+ * @file ntt_layer123_harness.c
+ * @brief Implements the proof harness for ntt_layer123 function.
+ */
+
+/*
+ * Insert project header files that
+ *   - include the declaration of the function
+ *   - include the types needed to declare function arguments
+ */
+#include <ntt.h>
+void ntt_layer123(int16_t *r);
+
+/**
+ * @brief Starting point for formal analysis
+ *
+ */
+void harness(void)
+{
+  int16_t *a;
+  ntt_layer123(a);
+}
