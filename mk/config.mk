a4c557ee1d632ad3fefd2aeac733065f1263fbb1
Hanno Becker <beckphan@amazon.co.uk>
2024-12-21 05:59:10 +0000

    Makefile: Only use `gcc-ar` if available
    Signed-off-by: Hanno Becker <beckphan@amazon.co.uk>

--- mk/config.mk
+++ mk/config.mk
@@ -12,20 +12,19 @@ CROSS_PREFIX ?=
 CC  ?= gcc
 CPP ?= cpp
 AR  ?= ar
-# NOTE: gcc-ar is a wrapper around ar that ensures proper integration with GCC plugins,
-# 		such as lto. Using gcc-ar is preferred when creating or linking static libraries
-# 		if the binary is compiled with -flto.
-# 		However, this doesn't apply to darwin as it is using clang instead, and there's no
-# 		gcc-ar wrapper as well.
-CC_AR ?= $(if $(findstring Darwin,$(shell uname -s)),ar,gcc-ar)
-
 CC  := $(CROSS_PREFIX)$(CC)
 CPP := $(CROSS_PREFIX)$(CPP)
 AR  := $(CROSS_PREFIX)$(AR)
-CC_AR  := $(CROSS_PREFIX)$(CC_AR)
 LD  := $(CC)
 OBJCOPY := $(CROSS_PREFIX)objcopy
 SIZE := $(CROSS_PREFIX)size
+
+# NOTE: gcc-ar is a wrapper around ar that ensures proper integration with GCC plugins,
+# 	such as lto. Using gcc-ar is preferred when creating or linking static libraries
+# 	if the binary is compiled with -flto. However, it is not universally present, so
+#       only use it if available.
+CC_AR ?= $(if $(and $(findstring gcc,$(shell $(CC) --version)), $(findstring gcc-ar, $(shell which $(CROSS_PREFIX)gcc-ar))),gcc-ar,ar)
+CC_AR  := $(CROSS_PREFIX)$(CC_AR)
 
 #################
 # Common config #
